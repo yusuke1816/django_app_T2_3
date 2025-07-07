426d16823fb13f1ddc3fd5efc0632877bead70d8
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-07-08 04:14:32 +0900

    デプロイテスト

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,11 @@ import os
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 # パスワードバリデーション
 AUTH_PASSWORD_VALIDATORS = [
