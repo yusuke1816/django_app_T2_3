93f795ca605309967c8d4a71d9bf25b6bb249a4b
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-07-09 02:01:42 +0900

    DB接続成功

--- backend/settings.py
+++ backend/settings.py
@@ -93,23 +93,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 import os
 import dj_database_url
+from decouple import config
+
+
+
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, conn_health_checks=True)
-    }
-else:
-    # ローカル環境用デフォルト（PostgreSQLの情報は自分の環境に合わせて）
- DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'money_manager_app',
-        'USER': 'tamurayusuke',
-        'PASSWORD': 'adminpass',
-         'HOST': '',  
-         'PORT': '', 
+        'USER': 'money_manager_app_user',
+        'PASSWORD': 'TelqzS7KMxWaevgUflZfA9YlV0PYciC9',
+        'HOST': 'dpg-d1m1ij7diees738vp1rg-a.oregon-postgres.render.com',  # ←フルホスト名に修正
+        'PORT': '5432',
     }
 }
 
